7d8883f07568a563245fc70a25656678108024fc
Ashanmukhasairam <shanmukhaagnihotram@gmail,com>
2025-01-28 07:19:16 +0530

    1second commit

--- Movie-Search-Project/vite-project/src/App.jsx
+++ Movie-Search-Project/vite-project/src/App.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useEffect,useState } from "react";
+import Search from "./components/search";
 
-export const App = () => {
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>App</div>
-  )
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> you'll Enjoy
+            without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <h1>{searchTerm}</h1>
+
+        <p>Search</p>
+      </div>
+    </main>
+  );
 }
+
+export default App;
