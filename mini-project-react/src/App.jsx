fa597574081fe86a1c1bd7f3ceb080b727e02ceb
Ashanmukhasairam <shanmukhaagnihotram@gmail.com>
2025-03-20 19:46:30 +0530

    first commit

---
+++ mini-project-react/src/App.jsx
@@ -0,0 +1,13 @@
+
+import SearchBox from "./SearchBox"
+function App() {
+  
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ mini-project-react/src/SearchBox.jsx
@@ -0,0 +1,8 @@
+export default function SearchBox() {
+    return (
+        <div>
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+        </div>
+    )
+    }
